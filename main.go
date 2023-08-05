094bdf04c5e5e554fda96ae7313f6682f08689d1
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-08-06 01:28:12 +0330

    init router

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 func main() {
 	_, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/go-shop?charset=utf8"))
 	if err != nil {
 		panic("failed to connect database")
+	}
+
+	// Routes
+	r := mux.NewRouter()
+
+	fmt.Println("Application run on port :8080")
+	if err = http.ListenAndServe(":8080", r); err != nil {
+		panic("Error when server application: " + err.Error())
 	}
 }
