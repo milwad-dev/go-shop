fa6a9ea59f36a448f13100c5c4596d0b205734ec
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-08-05 16:50:21 +0330

    wip

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,13 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
+	gorm.Model
+
 	ID              int
 	Name            string
 	Email           string
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func main() {
-	
+	_, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/go-shop?charset=utf8"))
+	if err != nil {
+		panic("failed to connect database")
+	}
 }
